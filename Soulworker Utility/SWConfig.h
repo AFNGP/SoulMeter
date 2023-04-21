839e5afe95a5b292a257486812d09d0ca12ba0ef
AFNGP <afn.swi@gmail.com>
2023-04-21 18:16:22 +0200

    Migrate to 64 GLB client and fix party and force crashes

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define APP_VERSION "1.4.1.1"
-#define SWMAGIC 7
+#define SWMAGIC 21
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
 #define USE_XOR 0
