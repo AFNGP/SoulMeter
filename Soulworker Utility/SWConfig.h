88c61431a21d521f7924ad2bfc3eebfb409d3a0d
AFNGP <afn.swi@gmail.com>
2023-09-08 14:51:50 +0300

    VP Fix Attempt

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.5.0.1"
+#define APP_VERSION "1.5.2.0"
 #define SWMAGIC 7
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
