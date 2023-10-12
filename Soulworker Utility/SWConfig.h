4d82dccf0d315e0cfe30b2ff8de0bb9fb586f7b7
AFNGP <afn.swi@gmail.com>
2023-10-12 18:23:33 +0200

    Fix for October 1st update

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define APP_VERSION "1.5.2.3"
-#define SWMAGIC 37
+#define APP_VERSION "1.5.2.4"
+#define SWMAGIC 38
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
 #define USE_XOR 0
