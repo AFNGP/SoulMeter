8b90149483862db16988075e5a95bbc05b133a0d
AFNGP <afn.swi@gmail.com>
2024-04-05 20:22:31 +0200

    Implement HH Hero - DB Update to include HH Hero - Meter will pause during immunity phases and ignore the DPS on the followers/crystals - Fixed display bug with M unit on char specific info

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.6.1.0"
+#define APP_VERSION "1.6.2.0"
 #define SWMAGIC 49
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
