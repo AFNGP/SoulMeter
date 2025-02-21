6ddc397a525e811ec536cb40fa15ac2cd1fed613
AFNGP <afn.swi@gmail.com>
2025-02-21 11:15:45 +0100

    Update DB for Helix

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.7.1.1"
+#define APP_VERSION "1.7.1.2"
 #define SWMAGIC 7
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
