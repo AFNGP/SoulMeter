173e2bc3bfbffe68233d1cd23b4f03a0193ae645
AFNGP <afn.swi@gmail.com>
2026-09-06 16:02:26 +0200

    Change version to 1.7.2.1

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.7.2.0"
+#define APP_VERSION "1.7.2.1"
 #define SWMAGIC 50
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
